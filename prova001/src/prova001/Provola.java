44cbdac157d6e6c038078efce47c1ef169837285
MarcoT <marco.tripolini@cercolavoro.com>
2023-07-12 21:56:00 +0200

    Prova di push

---
+++ prova001/src/prova001/Provola.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package prova001;
+
+/**
+ * @author marco
+ *
+ */
+public class Provola {
+
+	/**
+	 * 
+	 */
+	public Provola() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello!");
+	}
+
+}
